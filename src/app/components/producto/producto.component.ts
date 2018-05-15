b730d26b53ed87fdd54f70f24855cfc751455d3b
Jose Luis Torres <java.mx@gmail.com>
2018-05-15 15:51:18 -0400

    OK cambios en las imagenes del producto

--- src/app/components/producto/producto.component.ts
+++ src/app/components/producto/producto.component.ts
@@ -9,9 +9,11 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ProductoComponent {
   producto: any = undefined;
+  cod: string = undefined;
   constructor(private route: ActivatedRoute, private _ps: ProductosService) {
     route.params.subscribe(parametros => {
       _ps.cargar_producto(parametros['id']).subscribe(res => {
+        this.cod = parametros['id'];
         this.producto = res.json();
       });
     });
